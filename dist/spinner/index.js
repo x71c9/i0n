67891336b36d310e664bf83f18293cc93d7728a4
x71c9 <108585118+x71c9@users.noreply.github.com>
2023-08-21 13:50:34 +0200

    compiled maps.

--- dist/spinner/index.js
+++ dist/spinner/index.js
@@ -12,15 +12,17 @@ const ora_spinner = ora({
 export const spinner = {
     text: _set_text,
     start: _start,
-    stop: _stop
+    stop: _stop,
 };
 function _set_text(text) {
     let processed_text = text;
     const regex_new_line = new RegExp(`\n|\r\n|0x0D0A|0x0A|0x0D|0x0d0a|0x0a|0x0d|¶`, 'g');
     processed_text = processed_text.replace(regex_new_line, ' ');
-    processed_text = (processed_text.length > process.stdout.columns - 8) ?
-        processed_text.substring(0, Math.floor(process.stdout.columns - 8)) + '...' :
-        processed_text;
+    processed_text =
+        processed_text.length > process.stdout.columns - 8
+            ? processed_text.substring(0, Math.floor(process.stdout.columns - 8)) +
+                '...'
+            : processed_text;
     ora_spinner.text = processed_text;
 }
 function _start() {
